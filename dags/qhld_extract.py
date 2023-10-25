b3c3a6ae3f86c5aa8c5608ebd8dd494661450898
Alex Ahumada <projectsperminute@gmail.com>
2023-10-25 16:00:56 +0200

    Adds slack notifications with SlackAPIPostOperator

--- dags/qhld_extract.py
+++ dags/qhld_extract.py
@@ -1,9 +1,10 @@
 import json
 
+import os
 from airflow import DAG
 from airflow.contrib.operators.ssh_operator import SSHOperator
 from airflow.contrib.hooks.ssh_hook import SSHHook
-from airflow.providers.http.operators.http import SimpleHttpOperator
+from airflow.providers.slack.operators.slack_api import SlackAPIPostOperator
 from airflow.utils.dates import days_ago
 
 
@@ -15,13 +16,12 @@ with DAG(
 ) as dag:
 
     ssh = SSHHook(ssh_conn_id='qhld', key_file='./keys/pw_airflow')
-
-    slack_start = SimpleHttpOperator(
-        task_id='slack_start',
-        headers={"Content-Type": "application/json"},
-        http_conn_id="n8n_slack",
-        endpoint='webhook/868e2659-d6bd-407e-aa75-6a8ed4ebbd4c',
-        data=json.dumps({'message': 'Empezando el procesamiento diario de datos de QHLD.'})
+    
+    slack_start = SlackAPIPostOperator(
+        task_id='send_slack_message',
+        token=os.environ.get('SLACK_API_TOKEN'),
+        text='Empezando el procesamiento diario de datos de QHLD.',
+        channel='#tech',
     )
 
     extract_members = SSHOperator(
@@ -65,25 +65,23 @@ with DAG(
         command="docker exec tipi-engine python quickex.py stats",
         ssh_hook=ssh
     )
-
-    slack_end = SimpleHttpOperator(
-        task_id='slack_end',
-        headers={"Content-Type": "application/json"},
-        http_conn_id="n8n_slack",
-        endpoint='webhook/868e2659-d6bd-407e-aa75-6a8ed4ebbd4c',
-        data=json.dumps({'message': 'Fin del procesamiento diario de datos de QHLD.'})
+    
+    slack_end = SlackAPIPostOperator(
+        task_id='send_slack_message',
+        token=os.environ.get('SLACK_API_TOKEN'),
+        text='Fin del procesamiento diario de datos de QHLD.',
+        channel='#tech',
     )
-
-    notify_error = SimpleHttpOperator(
-        task_id='notify_error',
-        headers={"Content-Type": "application/json"},
-        http_conn_id="n8n_slack",
-        endpoint='webhook/868e2659-d6bd-407e-aa75-6a8ed4ebbd4c',
-        data=json.dumps({'message': 'Error durante la ejecucion.'}),
+    
+    notify_error = SlackAPIPostOperator(
+        task_id='send_slack_message',
+        token=os.environ.get('SLACK_API_TOKEN'),
+        text='Error durante la ejecucion (QHLD).',
+        channel='#tech',
         trigger_rule='one_failed'
     )
 
-    extract_members >> update_groups >> extract_initiatives >> extract_votes >> tag >> alerts >> stats
+    slack_start >> extract_members >> update_groups >> extract_initiatives >> extract_votes >> tag >> alerts >> stats >> slack_end >> notify_error
 
 if __name__ == "__main__":
     dag.cli()
