1ebb8eb414cda5e04f0acb11abcc3a31521fe5f7
Alex Ahumada <projectsperminute@gmail.com>
2023-11-08 18:15:49 +0100

    Fixes execution time slack notification

--- dags/qhld_extract.py
+++ dags/qhld_extract.py
@@ -1,5 +1,8 @@
+from datetime import datetime, timezone
 from airflow import DAG
-from airflow.models import Variable
+from airflow.models import Variable, TaskInstance
+from airflow.utils.state import State
+from airflow.operators.python import BranchPythonOperator
 from airflow.providers.ssh.operators.ssh import SSHOperator
 from airflow.providers.ssh.hooks.ssh import SSHHook
 from airflow.providers.slack.operators.slack import SlackAPIPostOperator
@@ -8,6 +11,34 @@ from airflow.utils.dates import days_ago
 
 SLACK_WEBHOOK_URL = Variable.get("SLACK_WEBHOOK_URL")
 SLACK_API_TOKEN = Variable.get("SLACK_API_TOKEN")
+
+
+def check_previous_tasks(**context):
+    dag = context["dag"]
+    tasks = dag.tasks
+    execution_date = context["execution_date"]
+    end_date = datetime.now(timezone.utc)
+    difference = end_date - execution_date
+    total_seconds = difference.total_seconds()
+    hours, remainder = divmod(total_seconds, 3600)
+    minutes, seconds = divmod(remainder, 60)
+    duration = "{:02}:{:02}:{:02}".format(int(hours), int(minutes), int(seconds))
+
+    context["ti"].xcom_push(
+        key="start_datetime", value=execution_date.strftime("%Y-%m-%d %H:%M:%S")
+    )
+    context["ti"].xcom_push(
+        key="end_datetime", value=end_date.strftime("%Y-%m-%d %H:%M:%S")
+    )
+    context["ti"].xcom_push(key="duration", value=duration)
+
+    for task in tasks:
+        if task.task_id == "slack_end_success" or task.task_id == "slack_end_failure":
+            continue
+        task_instance = TaskInstance(task, execution_date)
+        if task_instance.current_state() == State.FAILED:
+            return "slack_end_failure"
+    return "slack_end_success"
 
 
 with DAG(
@@ -21,28 +52,28 @@ with DAG(
         "icon_url": "https://politicalwatch.es/images/icons/icon_192px.png",
         "channel": "#tech",
     },
-    on_success_callback=[
-        send_slack_notification(
-            text=":large_green_circle: Sin errores en procesamiento diario de datos de QHLD. \
-            \n Start: {{ execution_date.strftime('%d-%m-%Y %H:%M:%S') }}.\
-            \n End: {{ next_execution_date.strftime('%d-%m-%Y %H:%M:%S') }}.\
-            \n Duration: {{ ((next_execution_date - execution_date).total_seconds() // 3600)|int }} horas {{ ((next_execution_date - execution_date).total_seconds() // 60 % 60)|int }} minutos {{ ((next_execution_date - execution_date).total_seconds() % 60)|int }} segundos.",
-            channel="#tech",
-            username="PW Notify",
-            icon_url="https://politicalwatch.es/images/icons/icon_192px.png",
-        )
-    ],
-    on_failure_callback=[
-        send_slack_notification(
-            text=":red_circle: Hay errores en procesamiento diario de datos de QHLD.\
-            \n Start: {{ execution_date.strftime('%d-%m-%Y %H:%M:%S') }}.\
-            \n End: {{ next_execution_date.strftime('%d-%m-%Y %H:%M:%S') }}.\
-            \n Duration: {{ ((next_execution_date - execution_date).total_seconds() // 3600)|int }} horas {{ ((next_execution_date - execution_date).total_seconds() // 60 % 60)|int }} minutos {{ ((next_execution_date - execution_date).total_seconds() % 60)|int }} segundos.",
-            channel="#tech",
-            username="PW Notify",
-            icon_url="https://politicalwatch.es/images/icons/icon_192px.png",
-        )
-    ],
+    # on_success_callback=[
+    #     send_slack_notification(
+    #         text=":large_green_circle: Sin errores en procesamiento diario de datos de QHLD. \
+    #         \n Start: {{ execution_date.strftime('%d-%m-%Y %H:%M:%S') }}.\
+    #         \n End: {{ next_execution_date.strftime('%d-%m-%Y %H:%M:%S') }}.\
+    #         \n Duration: {{ ((next_execution_date - execution_date).total_seconds() // 3600)|int }} horas {{ ((next_execution_date - execution_date).total_seconds() // 60 % 60)|int }} minutos {{ ((next_execution_date - execution_date).total_seconds() % 60)|int }} segundos.",
+    #         channel="#tech",
+    #         username="PW Notify",
+    #         icon_url="https://politicalwatch.es/images/icons/icon_192px.png",
+    #     )
+    # ],
+    # on_failure_callback=[
+    #     send_slack_notification(
+    #         text=":red_circle: Hay errores en procesamiento diario de datos de QHLD.\
+    #         \n Start: {{ execution_date.strftime('%d-%m-%Y %H:%M:%S') }}.\
+    #         \n End: {{ next_execution_date.strftime('%d-%m-%Y %H:%M:%S') }}.\
+    #         \n Duration: {{ ((next_execution_date - execution_date).total_seconds() // 3600)|int }} horas {{ ((next_execution_date - execution_date).total_seconds() // 60 % 60)|int }} minutos {{ ((next_execution_date - execution_date).total_seconds() % 60)|int }} segundos.",
+    #         channel="#tech",
+    #         username="PW Notify",
+    #         icon_url="https://politicalwatch.es/images/icons/icon_192px.png",
+    #     )
+    # ],
     tags=["pro", "qhld"],
 ) as dag:
     ssh = SSHHook(ssh_conn_id="qhld", key_file="./keys/pw_airflow", cmd_timeout=7200)
@@ -146,6 +177,27 @@ with DAG(
         ],
     )
 
+    branch = BranchPythonOperator(
+        task_id="branch",
+        python_callable=check_previous_tasks,
+        provide_context=True,
+        dag=dag,
+    )
+
+    slack_end_success = SlackAPIPostOperator(
+        task_id="slack_end_success",
+        text=":large_green_circle: Sin errores en procesamiento diario de datos de QHLD. \
+        \n Tiempo de ejecución: {{ ti.xcom_pull(key='duration') }}",
+        # \n Running: {{ ti.xcom_pull(key='start_datetime') }} => {{ ti.xcom_pull(key='end_datetime') }} \
+        dag=dag,
+    )
+
+    slack_end_failure = SlackAPIPostOperator(
+        task_id="slack_end_failure",
+        text=":red_circle: Hay errores en procesamiento diario de datos de QHLD.",
+        dag=dag,
+    )
+
     (
         extract_members
         >> update_groups
@@ -154,6 +206,8 @@ with DAG(
         >> tag
         >> alerts
         >> stats
+        >> branch
+        >> [slack_end_success, slack_end_failure]
     )
 
 if __name__ == "__main__":
--- dags/rtve_extract.py
+++ dags/rtve_extract.py
@@ -1,5 +1,8 @@
+from datetime import datetime, timezone
 from airflow import DAG
-from airflow.models import Variable
+from airflow.models import Variable, TaskInstance
+from airflow.utils.state import State
+from airflow.operators.python import BranchPythonOperator
 from airflow.providers.ssh.operators.ssh import SSHOperator
 from airflow.providers.ssh.hooks.ssh import SSHHook
 from airflow.providers.slack.operators.slack import SlackAPIPostOperator
@@ -7,6 +10,35 @@ from airflow.providers.slack.notifications.slack import send_slack_notification
 from airflow.utils.dates import days_ago
 
 SLACK_API_TOKEN = Variable.get("SLACK_API_TOKEN")
+
+
+def check_previous_tasks(**context):
+    dag = context["dag"]
+    tasks = dag.tasks
+    execution_date = context["execution_date"]
+    end_date = datetime.now(timezone.utc)
+    difference = end_date - execution_date
+    total_seconds = difference.total_seconds()
+    hours, remainder = divmod(total_seconds, 3600)
+    minutes, seconds = divmod(remainder, 60)
+    duration = "{:02}:{:02}:{:02}".format(int(hours), int(minutes), int(seconds))
+
+    context["ti"].xcom_push(
+        key="start_datetime", value=execution_date.strftime("%Y-%m-%d %H:%M:%S")
+    )
+    context["ti"].xcom_push(
+        key="end_datetime", value=end_date.strftime("%Y-%m-%d %H:%M:%S")
+    )
+    context["ti"].xcom_push(key="duration", value=duration)
+
+    for task in tasks:
+        if task.task_id == "slack_end_success" or task.task_id == "slack_end_failure":
+            continue
+        task_instance = TaskInstance(task, execution_date)
+        if task_instance.current_state() == State.FAILED:
+            return "slack_end_failure"
+    return "slack_end_success"
+
 
 with DAG(
     dag_id="rtve_extract",
@@ -19,28 +51,28 @@ with DAG(
         "icon_url": "https://politicalwatch.es/images/icons/icon_192px.png",
         "channel": "#tech",
     },
-    on_success_callback=[
-        send_slack_notification(
-            text=":large_green_circle: Sin errores en la extracción de subtítulos de RTVE.\
-            \n Start: {{ execution_date.strftime('%d-%m-%Y %H:%M:%S') }}.\
-            \n End: {{ next_execution_date.strftime('%d-%m-%Y %H:%M:%S') }}.\
-            \n Duration: {{ ((next_execution_date - execution_date).total_seconds() // 3600)|int }} horas {{ ((next_execution_date - execution_date).total_seconds() // 60 % 60)|int }} minutos {{ ((next_execution_date - execution_date).total_seconds() % 60)|int }} segundos.",
-            channel="#tech",
-            username="PW Notify",
-            icon_url="https://politicalwatch.es/images/icons/icon_192px.png",
-        )
-    ],
-    on_failure_callback=[
-        send_slack_notification(
-            text=":red_circle: Hay errores en la extracción de subtítulos de RTVE.\
-            \n Start: {{ execution_date.strftime('%d-%m-%Y %H:%M:%S') }}.\
-            \n End: {{ next_execution_date.strftime('%d-%m-%Y %H:%M:%S') }}.\
-            \n Duration: {{ ((next_execution_date - execution_date).total_seconds() // 3600)|int }} horas {{ ((next_execution_date - execution_date).total_seconds() // 60 % 60)|int }} minutos {{ ((next_execution_date - execution_date).total_seconds() % 60)|int }} segundos.",
-            channel="#tech",
-            username="PW Notify",
-            icon_url="https://politicalwatch.es/images/icons/icon_192px.png",
-        )
-    ],
+    # on_success_callback=[
+    #     send_slack_notification(
+    #         text=":large_green_circle: Sin errores en la extracción de subtítulos de RTVE.\
+    #         \n Start: {{ execution_date.strftime('%d-%m-%Y %H:%M:%S') }}.\
+    #         \n End: {{ next_execution_date.strftime('%d-%m-%Y %H:%M:%S') }}.\
+    #         \n Duration: {{ ((next_execution_date - execution_date).total_seconds() // 3600)|int }} horas {{ ((next_execution_date - execution_date).total_seconds() // 60 % 60)|int }} minutos {{ ((next_execution_date - execution_date).total_seconds() % 60)|int }} segundos.",
+    #         channel="#tech",
+    #         username="PW Notify",
+    #         icon_url="https://politicalwatch.es/images/icons/icon_192px.png",
+    #     )
+    # ],
+    # on_failure_callback=[
+    #     send_slack_notification(
+    #         text=":red_circle: Hay errores en la extracción de subtítulos de RTVE.\
+    #         \n Start: {{ execution_date.strftime('%d-%m-%Y %H:%M:%S') }}.\
+    #         \n End: {{ next_execution_date.strftime('%d-%m-%Y %H:%M:%S') }}.\
+    #         \n Duration: {{ ((next_execution_date - execution_date).total_seconds() // 3600)|int }} horas {{ ((next_execution_date - execution_date).total_seconds() // 60 % 60)|int }} minutos {{ ((next_execution_date - execution_date).total_seconds() % 60)|int }} segundos.",
+    #         channel="#tech",
+    #         username="PW Notify",
+    #         icon_url="https://politicalwatch.es/images/icons/icon_192px.png",
+    #     )
+    # ],
     tags=["pro", "rtve"],
 ) as dag:
     ssh = SSHHook(ssh_conn_id="rtve", key_file="./keys/pw_airflow", cmd_timeout=7200)
@@ -67,7 +99,28 @@ with DAG(
         ],
     )
 
-    api_extract >> calculate_stats
+    branch = BranchPythonOperator(
+        task_id="branch",
+        python_callable=check_previous_tasks,
+        provide_context=True,
+        dag=dag,
+    )
+
+    slack_end_success = SlackAPIPostOperator(
+        task_id="slack_end_success",
+        text=":large_green_circle: Sin errores en la extracción de subtítulos de RTVE. \
+        \n Tiempo de ejecución: {{ ti.xcom_pull(key='duration') }}",
+        # \n Running: {{ ti.xcom_pull(key='start_datetime') }} => {{ ti.xcom_pull(key='end_datetime') }} \
+        dag=dag,
+    )
+
+    slack_end_failure = SlackAPIPostOperator(
+        task_id="slack_end_failure",
+        text=":red_circle: Hay errores en la extracción de subtítulos de RTVE.",
+        dag=dag,
+    )
+
+    api_extract >> calculate_stats >> branch >> [slack_end_success, slack_end_failure]
 
 if __name__ == "__main__":
     dag.cli()
