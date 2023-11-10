41b54dc303e3064118600eab19b9f9d90edc0bf6
Alex Ahumada <projectsperminute@gmail.com>
2023-11-10 14:06:24 +0100

    Adds backup workflow to qhld and rtve

--- dags/qhld_extract.py
+++ dags/qhld_extract.py
@@ -1,3 +1,10 @@
+import fnmatch
+import os
+from datetime import datetime, timezone
+from google.oauth2.service_account import Credentials
+from googleapiclient.http import MediaFileUpload
+from googleapiclient.discovery import build
+
 from datetime import datetime, timezone
 from airflow import DAG
 from airflow.models import Variable, TaskInstance
@@ -5,11 +12,14 @@ from airflow.utils.state import State
 from airflow.operators.python import PythonOperator, BranchPythonOperator
 from airflow.providers.ssh.operators.ssh import SSHOperator
 from airflow.providers.ssh.hooks.ssh import SSHHook
+from airflow.providers.sftp.operators.sftp import SFTPOperator
 from airflow.providers.slack.operators.slack import SlackAPIPostOperator
 from airflow.providers.slack.notifications.slack import send_slack_notification
 from airflow.utils.dates import days_ago
 
 SLACK_API_TOKEN = Variable.get("SLACK_API_TOKEN")
+QHLD_DRIVE_FOLDER_ID = Variable.get("QHLD_DRIVE_FOLDER_ID")
+RTVE_DRIVE_FOLDER_ID = Variable.get("RTVE_DRIVE_FOLDER_ID")
 
 
 def get_dag_metadata(**context):
@@ -44,6 +54,35 @@ def check_previous_tasks(**context):
         if task_instance.current_state() == State.FAILED:
             return "slack_end_failure"
     return "slack_end_success"
+
+
+def keep_last_two_files(directory: str, pattern: str):
+    files = sorted(
+        (
+            os.path.join(directory, filename)
+            for filename in os.listdir(directory)
+            if fnmatch.fnmatch(filename, pattern)
+        ),
+        key=os.path.basename,
+    )
+
+    # Delete all but the last two files
+    for file in files[:-2]:
+        os.remove(file)
+
+
+def upload_to_drive(filepath, filename, folder_id):
+    creds = Credentials.from_service_account_file("./keys/credentials.json")
+    service = build("drive", "v3", credentials=creds)
+
+    file_metadata = {"name": filename, "parents": [folder_id]}
+    media = MediaFileUpload(filepath, resumable=True)
+    file = (
+        service.files()
+        .create(body=file_metadata, media_body=media, fields="id")
+        .execute()
+    )
+    print("File ID: %s" % file.get("id"))
 
 
 with DAG(
@@ -209,6 +248,107 @@ with DAG(
         dag=dag,
     )
 
+    qhld_dump = SSHOperator(
+        task_id="qhld_dump",
+        ssh_hook=ssh,
+        cmd_timeout=7200,
+        command="""
+            docker exec tipi-mongo  mongodump --username tipi --password tipi --db tipidb --archive > ~/backups/tipidb-daily-$(date +%Y-%m-%d).gz
+        """,
+        on_success_callback=[
+            send_slack_notification(
+                text=":large_green_circle: La tarea QHLD: {{ ti.task_id }} ha finalizado correctamente.",
+                channel="#tech",
+                username="PW Notify",
+                icon_url="https://politicalwatch.es/images/icons/icon_192px.png",
+            )
+        ],
+        on_failure_callback=[
+            send_slack_notification(
+                text=":warning: La tarea QHLD: {{ ti.task_id }} ha fallado.",
+                channel="#tech",
+                username="PW Notify",
+                icon_url="https://politicalwatch.es/images/icons/icon_192px.png",
+            )
+        ],
+    )
+
+    qhld_copy_bkp = SFTPOperator(
+        task_id="qhld_copy_bkp",
+        ssh_hook=ssh,
+        local_filepath="/home/airflow/backups/qhld/tipidb-daily-{{ ds }}.gz",
+        remote_filepath="/home/ubuntu/backups/tipidb-daily-{{ ds }}.gz",
+        operation="get",
+        create_intermediate_dirs=True,
+        # on_success_callback=[
+        #     send_slack_notification(
+        #         text=":large_green_circle: La tarea QHLD: {{ ti.task_id }} ha finalizado correctamente.",
+        #         channel="#tech",
+        #         username="PW Notify",
+        #         icon_url="https://politicalwatch.es/images/icons/icon_192px.png",
+        #     )
+        # ],
+        on_failure_callback=[
+            send_slack_notification(
+                text=":warning: La tarea QHLD: {{ ti.task_id }} ha fallado.",
+                channel="#tech",
+                username="PW Notify",
+                icon_url="https://politicalwatch.es/images/icons/icon_192px.png",
+            )
+        ],
+    )
+
+    qhld_to_drive = PythonOperator(
+        task_id="qhld_to_drive",
+        python_callable=upload_to_drive,
+        op_kwargs={
+            "filepath": "/home/airflow/backups/qhld/tipidb-daily-{{ ds }}.gz",
+            "filename": "tipidb-daily-{{ ds }}.gz",
+            "folder_id": QHLD_DRIVE_FOLDER_ID,
+        },
+        # on_success_callback=[
+        #     send_slack_notification(
+        #         text=":large_green_circle: La tarea QHLD: {{ ti.task_id }} ha finalizado correctamente.",
+        #         channel="#tech",
+        #         username="PW Notify",
+        #         icon_url="https://politicalwatch.es/images/icons/icon_192px.png",
+        #     )
+        # ],
+        on_failure_callback=[
+            send_slack_notification(
+                text=":warning: La tarea QHLD: {{ ti.task_id }} ha fallado.",
+                channel="#tech",
+                username="PW Notify",
+                icon_url="https://politicalwatch.es/images/icons/icon_192px.png",
+            )
+        ],
+    )
+
+    qhld_delete_old = PythonOperator(
+        task_id="qhld_delete_old",
+        python_callable=keep_last_two_files,
+        op_kwargs={
+            "directory": "/home/airflow/backups/qhld",
+            "pattern": "tipidb-daily-????-??-??.gz",
+        },
+        # on_success_callback=[
+        #     send_slack_notification(
+        #         text=":large_green_circle: La tarea QHLD: {{ ti.task_id }} ha finalizado correctamente.",
+        #         channel="#tech",
+        #         username="PW Notify",
+        #         icon_url="https://politicalwatch.es/images/icons/icon_192px.png",
+        #     )
+        # ],
+        on_failure_callback=[
+            send_slack_notification(
+                text=":warning: La tarea QHLD: {{ ti.task_id }} ha fallado.",
+                channel="#tech",
+                username="PW Notify",
+                icon_url="https://politicalwatch.es/images/icons/icon_192px.png",
+            )
+        ],
+    )
+
     (
         xcom_metadata
         >> extract_members
@@ -220,6 +360,10 @@ with DAG(
         >> stats
         >> branch
         >> [slack_end_success, slack_end_failure]
+        >> qhld_dump
+        >> qhld_copy_bkp
+        >> qhld_to_drive
+        >> qhld_delete_old
     )
 
 if __name__ == "__main__":
--- dags/rtve_extract.py
+++ dags/rtve_extract.py
@@ -1,15 +1,24 @@
+import fnmatch
+import os
 from datetime import datetime, timezone
+from google.oauth2.service_account import Credentials
+from googleapiclient.http import MediaFileUpload
+from googleapiclient.discovery import build
+
 from airflow import DAG
 from airflow.models import Variable, TaskInstance
 from airflow.utils.state import State
 from airflow.operators.python import PythonOperator, BranchPythonOperator
 from airflow.providers.ssh.operators.ssh import SSHOperator
 from airflow.providers.ssh.hooks.ssh import SSHHook
+from airflow.providers.sftp.operators.sftp import SFTPOperator
 from airflow.providers.slack.operators.slack import SlackAPIPostOperator
 from airflow.providers.slack.notifications.slack import send_slack_notification
 from airflow.utils.dates import days_ago
 
 SLACK_API_TOKEN = Variable.get("SLACK_API_TOKEN")
+QHLD_DRIVE_FOLDER_ID = Variable.get("QHLD_DRIVE_FOLDER_ID")
+RTVE_DRIVE_FOLDER_ID = Variable.get("RTVE_DRIVE_FOLDER_ID")
 
 
 def get_dag_metadata(**context):
@@ -44,6 +53,35 @@ def check_previous_tasks(**context):
         if task_instance.current_state() == State.FAILED:
             return "slack_end_failure"
     return "slack_end_success"
+
+
+def keep_last_two_files(directory: str, pattern: str):
+    files = sorted(
+        (
+            os.path.join(directory, filename)
+            for filename in os.listdir(directory)
+            if fnmatch.fnmatch(filename, pattern)
+        ),
+        key=os.path.basename,
+    )
+
+    # Delete all but the last two files
+    for file in files[:-2]:
+        os.remove(file)
+
+
+def upload_to_drive(filepath, filename, folder_id):
+    creds = Credentials.from_service_account_file("./keys/credentials.json")
+    service = build("drive", "v3", credentials=creds)
+
+    file_metadata = {"name": filename, "parents": [folder_id]}
+    media = MediaFileUpload(filepath, resumable=True)
+    file = (
+        service.files()
+        .create(body=file_metadata, media_body=media, fields="id")
+        .execute()
+    )
+    print("File ID: %s" % file.get("id"))
 
 
 with DAG(
@@ -132,12 +170,117 @@ with DAG(
         dag=dag,
     )
 
+    rtve_dump = SSHOperator(
+        task_id="rtve_dump",
+        ssh_hook=ssh,
+        cmd_timeout=7200,
+        command="""
+            docker exec rtve2030-postgres-1 pg_dump -U rtve -d rtve > ~/backups/rtve-daily-$(date +%Y-%m-%d).sql
+        """,
+        on_success_callback=[
+            send_slack_notification(
+                text=":large_green_circle: La tarea RTVE: {{ ti.task_id }} ha finalizado correctamente.",
+                channel="#tech",
+                username="PW Notify",
+                icon_url="https://politicalwatch.es/images/icons/icon_192px.png",
+            )
+        ],
+        on_failure_callback=[
+            send_slack_notification(
+                text=":warning: La tarea RTVE: {{ ti.task_id }} ha fallado.",
+                channel="#tech",
+                username="PW Notify",
+                icon_url="https://politicalwatch.es/images/icons/icon_192px.png",
+            )
+        ],
+    )
+
+    rtve_copy_bkp = SFTPOperator(
+        task_id="rtve_copy_bkp",
+        ssh_hook=ssh,
+        local_filepath="/home/airflow/backups/rtve/rtve-daily-{{ ds }}.sql",
+        remote_filepath="/home/ubuntu/backups/rtve-daily-{{ ds }}.sql",
+        operation="get",
+        create_intermediate_dirs=True,
+        # on_success_callback=[
+        #     send_slack_notification(
+        #         text=":large_green_circle: La tarea RTVE: {{ ti.task_id }} ha finalizado correctamente.",
+        #         channel="#tech",
+        #         username="PW Notify",
+        #         icon_url="https://politicalwatch.es/images/icons/icon_192px.png",
+        #     )
+        # ],
+        on_failure_callback=[
+            send_slack_notification(
+                text=":warning: La tarea RTVE: {{ ti.task_id }} ha fallado.",
+                channel="#tech",
+                username="PW Notify",
+                icon_url="https://politicalwatch.es/images/icons/icon_192px.png",
+            )
+        ],
+    )
+
+    rtve_to_drive = PythonOperator(
+        task_id="rtve_to_drive",
+        python_callable=upload_to_drive,
+        op_kwargs={
+            "filepath": "/home/airflow/backups/rtve/rtve-daily-{{ ds }}.sql",
+            "filename": "rtve-daily-{{ ds }}.sql",
+            "folder_id": RTVE_DRIVE_FOLDER_ID,
+        },
+        # on_success_callback=[
+        #     send_slack_notification(
+        #         text=":large_green_circle: La tarea RTVE: {{ ti.task_id }} ha finalizado correctamente.",
+        #         channel="#tech",
+        #         username="PW Notify",
+        #         icon_url="https://politicalwatch.es/images/icons/icon_192px.png",
+        #     )
+        # ],
+        on_failure_callback=[
+            send_slack_notification(
+                text=":warning: La tarea RTVE: {{ ti.task_id }} ha fallado.",
+                channel="#tech",
+                username="PW Notify",
+                icon_url="https://politicalwatch.es/images/icons/icon_192px.png",
+            )
+        ],
+    )
+
+    rtve_delete_old = PythonOperator(
+        task_id="rtve_delete_old",
+        python_callable=keep_last_two_files,
+        op_kwargs={
+            "directory": "/home/airflow/backups/rtve",
+            "pattern": "rtve-daily-????-??-??.sql",
+        },
+        # on_success_callback=[
+        #     send_slack_notification(
+        #         text=":large_green_circle: La tarea QHLD: {{ ti.task_id }} ha finalizado correctamente.",
+        #         channel="#tech",
+        #         username="PW Notify",
+        #         icon_url="https://politicalwatch.es/images/icons/icon_192px.png",
+        #     )
+        # ],
+        on_failure_callback=[
+            send_slack_notification(
+                text=":warning: La tarea QHLD: {{ ti.task_id }} ha fallado.",
+                channel="#tech",
+                username="PW Notify",
+                icon_url="https://politicalwatch.es/images/icons/icon_192px.png",
+            )
+        ],
+    )
+
     (
         xcom_metadata
         >> api_extract
         >> calculate_stats
         >> branch
         >> [slack_end_success, slack_end_failure]
+        >> rtve_dump
+        >> rtve_copy_bkp
+        >> rtve_to_drive
+        >> rtve_delete_old
     )
 
 if __name__ == "__main__":
